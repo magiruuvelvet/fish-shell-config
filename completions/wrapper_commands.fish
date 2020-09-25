be3f66e19f65a833d3a4c677abf2aa8b86cdf81b
magiruuvelvet <magiruuvelvet@naver.com>
2020-09-25 13:16:17 +0200

    2020-09-25-13:16

--- completions/wrapper_commands.fish
+++ completions/wrapper_commands.fish
@@ -5,3 +5,8 @@ end
 
 # list of wrapper commands to forward completions
 complete -c "vpn-run" -x -a "(__fish_complete_wrapper_command_subcommand)"
+complete -c "primerun" -x -a "(__fish_complete_wrapper_command_subcommand)"
+complete -c "primusrun" -x -a "(__fish_complete_wrapper_command_subcommand)"
+complete -c "optirun" -x -a "(__fish_complete_wrapper_command_subcommand)"
+complete -c "mangohud" -x -a "(__fish_complete_wrapper_command_subcommand)"
+complete -c "primerun-hud" -x -a "(__fish_complete_wrapper_command_subcommand)"
